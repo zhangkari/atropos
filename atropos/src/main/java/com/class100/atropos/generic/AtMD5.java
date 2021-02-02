346f8beaf412a36f90c422a46115a8d388dd160b
Karic <zhangkaric@gmail.com>
2021-02-02 15:07:38 +0800

    Release libAtropos v0.0.5

--- atropos/src/main/java/com/class100/atropos/generic/AtMD5.java
+++ atropos/src/main/java/com/class100/atropos/generic/AtMD5.java
@@ -1,5 +1,6 @@
 package com.class100.atropos.generic;
 
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.UnsupportedEncodingException;
@@ -9,14 +10,10 @@ public class AtMD5 extends AtAbilityAdapter {
         return DigestUtils.md5(bytes);
     }
 
-    public static String md5(String content) {
-        return DigestUtils.md5Hex(content);
-    }
-
     public static String signYsxApi(String text, String key, String charset) {
         text = text + key;
         try {
-            return DigestUtils.md5Hex(getBytes(text, charset));
+            return new String(Hex.encodeHex(DigestUtils.md5(getBytes(text, charset))));
         } catch (Exception e) {
             return null;
         }
